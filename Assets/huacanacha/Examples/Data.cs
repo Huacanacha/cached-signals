54ae59f09decfd237005ecdba8bc7b94839c0026
Jason MacRae <jmacrae@Jasons-MacBook-Pro.local>
2019-01-06 19:11:47 -0800

    NEW: Examples WIP

---
+++ Assets/huacanacha/Examples/Data.cs
@@ -0,0 +1,10 @@
+﻿namespace huacanacha.Examples.signal {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class Data : MonoBehaviour {
+
+    }
+
+}
